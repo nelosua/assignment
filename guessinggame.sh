6933ae6f1ed10230c099b509aa9c9e4c51d7a4de
nelosua <nelosua@email.com>
2019-11-19 11:35:44 +0100

    final assignment

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,22 +1,25 @@
-function countingfiles {
-ls $(pwd) | wc -l
-}
-
-count=$(countingfiles)
-
 echo "How many files do you think are in the current directory?"
+echo "(Type in a number and press enter)"
+
+function guessinggame {
+
+count=$(ls $(pwd) | wc -l)
+
 read number
 
 while  [[ $number -ne count ]]
 do
-if [[ $number -gt count ]]
-then
-	echo "Sorry, your guess is too high, try again:"
-	read number				
-else
-	echo "Sorry, your guess is too low, try again:"
-	read number
-fi
+	if [[ $number -gt count ]]
+	then
+		echo "Sorry, your guess is too high, try again:"
+		read number				
+	else
+		echo "Sorry, your guess is too low, try again:"
+		read number
+	fi
 done
 
 echo "Congratulations, there are $count files in the directory!"
+}
+
+guessinggame
